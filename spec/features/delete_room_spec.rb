ef1ef03734c0229d81f4967200e90268f39ff405
Collin Jilbert <cjilbert504@gmail.com>
2020-10-30 18:39:40 -0500

    Add spec for deleting a room record

---
+++ spec/features/delete_room_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "Deleting a room" do
+	it "destroys the room and shows the index page without the deleted room" do
+		room = Room.create(studio_number: 1)
+
+		visit room_url(room)
+		click_link "Delete Room"
+
+		expect(current_path).to eq(rooms_path)
+		expect(page).not_to have_text("Room #1")
+	end
+end
