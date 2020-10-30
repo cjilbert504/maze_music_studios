119a64163884b89bcf8d1ce9692ccaff8c144df3
Collin Jilbert <cjilbert504@gmail.com>
2020-10-30 18:18:47 -0500

    Add feature to cancel editing a room

--- spec/features/navigate_room_spec.rb
+++ spec/features/navigate_room_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Navigating rooms" do
-	it "Allows navigation between the index page and the show page" do
+	it "allows navigation between the index page and the show page" do
 		room = Room.create(studio_number: 1)
 
 		visit rooms_url
@@ -10,12 +10,21 @@ RSpec.describe "Navigating rooms" do
 		expect(current_path).to eq(room_path(room))
 	end
 
-	it "Allows navigation from the show page to the index page" do
+	it "allows navigation from the show page to the index page" do
 		room = Room.create(studio_number: 1)
 
 		visit room_url(room)
 		click_link "Back To All Rooms"
 
 		expect(current_path).to eq(rooms_path)
+	end
+
+	it "allows the ability to cancel editing a room and returns to current room's show page" do
+		room = Room.create(studio_number: 1)
+
+		visit edit_room_url(room)
+		click_link "Cancel Editing"
+
+		expect(current_path).to eq(room_path(room))
 	end
 end
