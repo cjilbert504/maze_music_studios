9c2c3c88c0d3d33ee8242a401f8b724b441d7d75
Collin Jilbert <cjilbert504@gmail.com>
2020-10-30 19:32:54 -0500

    Add editing a room's studio number to edit room spec

--- spec/features/edit_room_spec.rb
+++ spec/features/edit_room_spec.rb
@@ -10,14 +10,18 @@ RSpec.describe "Editing a room" do
 		expect(current_path).to eq(edit_room_path(room))
 		expect(page).to have_text("Editing: Room #1")
 
+		expect(find_field("room_studio_number").value).to eq(room.studio_number.to_s)
 		expect(find_field("room_occupancy_status").value).to eq(room.occupancy_status)
+		expect(find_field("room_availability_status").value).to eq(room.availability_status)
 
+		select "2", :from => "room_studio_number"
 		select "occupied", :from => "room_occupancy_status"
 		select "unavailable", :from => "room_availability_status"
 
 		click_button "Update Room"
 
 		expect(current_path).to eq(room_path(room))
+		expect(page).to have_text("Room #2")
 		expect(page).to have_text("occupied")
 		expect(page).to have_text("unavailable")
 	end
