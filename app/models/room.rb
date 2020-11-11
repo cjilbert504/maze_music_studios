7c7bf5fc989ee9246594ac98065b4ed66b453225
Collin Jilbert <cjilbert504@gmail.com>
2020-11-11 08:23:03 -0600

    Refactor room edit form to use enums

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,4 @@
 class Room < ApplicationRecord
   enum occupancy_status: %i[vacant occupied]
   enum availability_status: %i[available unavailable]
-
-  OCCUPANCY_OPTIONS = %w[vacant occupied].freeze
-  AVAILABILITY_OPTIONS = %w[available unavailable].freeze
 end
