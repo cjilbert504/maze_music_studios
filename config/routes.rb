c609887c539920cb4ff2bcf048730506138c2eef
Collin Jilbert <cjilbert504@gmail.com>
2021-01-07 14:52:08 -0600

    start new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   root 'static#home'
   resources :rooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -20,19 +20,4 @@ ActiveRecord::Schema.define(version: 20201118193255) do
     t.integer "studio_number"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "phone_number", default: "", null: false
-    t.string "address", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
